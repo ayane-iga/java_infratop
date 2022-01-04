b6c94c6534a59670a82e15282991bf392ef7cf5f
EC2 Default User <ec2-user@ip-172-31-2-245.ec2.internal>
2022-01-04 04:21:20 +0000

    java ~5まで

---
+++ Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+        long long1 = 551;
+        System.out.println(long1);
+        double double1 = 1.11;
+        System.out.println(double1);
+        boolean boolean1 = true;
+        System.out.println(boolean1);
+        
+        Float float1 = 1.1f;
+        System.out.println(float1);
+        Integer integer1 = 551;
+        System.out.println(integer1);
+        Character character1 = 'i';
+        System.out.println(character1);
+        
+        int[] array1 = { 10, 20, 30, 40, 50 };
+        System.out.println(array1[3]);
+    }
+}
---
+++ Ready01.java
@@ -0,0 +1,20 @@
+public class Ready01 {
+    public static void main(String[] args) {
+        int num1;
+        String str1;
+        num1 = 10;
+        str1 = "いがさき";
+        //System.out.println(num1);
+        System.out.println(str1);
+
+        str1 = "あしたに";
+        System.out.println(str1);
+        
+        final String lucky_number;
+        lucky_number = "ラッキーナンバー";
+        System.out.println(lucky_number);
+
+        //int num2 = 30;
+        //System.out.println(num2);
+    }
+}
